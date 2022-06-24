f6da86f0ddb637eee093fdd31825e9fc988b7f6f
Viacheslav <maverik2047@yandex.ru>
2022-06-24 18:44:58 +0300

    folder added

--- src/test/java/tests/AutomationFormTest.java
+++ src/test/java/tests/AutomationFormTest.java
@@ -20,6 +20,7 @@ public class AutomationFormTest extends TestBase {
     @Story("Создание новой сборки в Jenkins")
     @DisplayName("Автозаполнение формы регистрации")
     @Link(name = "Demoqa", url = "https://demoqa.com/automation-practice-form")
+
     void successfulTest() {
         String fName = "Mark";
         String lName = "Born";

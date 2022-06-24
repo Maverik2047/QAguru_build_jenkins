ee6dd3a9a525d8be55d753cd266f65f53ba8c0c4
Viacheslav <maverik2047@yandex.ru>
2022-06-24 16:29:24 +0300

    new repository for Jenkins

--- src/test/java/AutomationFormTest.java
+++ src/test/java/AutomationFormTest.java
@@ -1,57 +1,80 @@
-import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.BeforeAll;
+import io.qameta.allure.*;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
 
 
-public class AutomationFormTest {
-    @BeforeAll
-    static void beforeAll() {
-        Configuration.baseUrl = " https://demoqa.com";
-        Configuration.browserSize = "1800x900";
-        Configuration.holdBrowserOpen = true;
-    }
+public class AutomationFormTest extends TestBase {
 
 
     @Test
+    @Owner("Maverik2047")
+    @Severity(SeverityLevel.NORMAL)
+    @Feature("Сборка в Jenkins")
+    @Story("Создание новой сборки в Jenkins")
+    @DisplayName("Автозаполнение формы регистрации")
+    @Link(name = "Demoqa", url = "https://demoqa.com/automation-practice-form")
+
     void successfulTest() {
-        open("/automation-practice-form");
+        String fName = "Mark";
+        String lName = "Born";
+        String userEmail = "Born@gloom.snail";
 
-        executeJavaScript("$('footer').remove()");
-        executeJavaScript("$('#fixedban').remove()");
+        step("Открыть форму регистрации студента", () -> {
+            open("/automation-practice-form");
+            executeJavaScript("$('footer').remove()");
+            executeJavaScript("$('#fixedban').remove()");
+        });
+        step("Заполнить поле Имя и Фамилия", () -> {
+            $("#firstName").setValue(fName);
+            $("#lastName").setValue(lName);
+        });
+        step("Заполнить поле Email", () -> {
+            $("#userEmail").setValue(userEmail);
+        });
+        step("Выбрать пол студента", () -> {
+            $("#genterWrapper").$(byText("Male")).click();
+        });
+        step("Ввести номер моб.телефона", () -> {
+            $("#userNumber").setValue("9500267340");
+        });
+        step("Ввести дату рождения", () -> {
+            $("#dateOfBirthInput").click();
+            $(".react-datepicker__month-select").selectOption("December");
+            $(".react-datepicker__year-select").selectOption("1988");
+            $("[aria-label='Choose Saturday, December 10th, 1988']").click();
+        });
+        step("Заполнить поле предметной области", () -> {
+            $("#subjectsInput").sendKeys("m");
+            $(byText("Maths")).click();
+        });
+        step("Выбрать хобби", () -> {
+            $("#hobbiesWrapper").$(byText("Sports")).click();
+        });
+        step("Загрузить фото", () -> {
+            $("#uploadPicture").uploadFromClasspath("test.png");
+        });
+        step("Ввести адрес,выбрать штат и город", () -> {
+            $("#currentAddress").setValue("Palm Street");
+            $("#state").click();
+            $(byText("NCR")).click();
+            $("#city").click();
+            $(byText("Noida")).click();
+        });
+        step("Нажать кнопку подтвердить", () -> {
+            $("#submit").click();
+        });
 
-        $("#firstName").setValue("Mark");
-        $("#lastName").setValue("Born");
-        $("#userEmail").setValue("Born@gloom.snail");
-        $("#genterWrapper").$(byText("Male")).click();
-        $("#userNumber").setValue("9500267340");
-
-        $("#dateOfBirthInput").click();
-        $(".react-datepicker__month-select").selectOption("December");
-        $(".react-datepicker__year-select").selectOption("1988");
-        $("[aria-label='Choose Saturday, December 10th, 1988']").click();
-
-        $("#hobbiesWrapper").$(byText("Sports")).click();
-        $("#uploadPicture").uploadFromClasspath("test.png");
-
-        $("#subjectsInput").sendKeys("m");
-        $(byText("Maths")).click();
-        $("#currentAddress").setValue("Palm Street");
-
-        $("#state").click();
-        $(byText("NCR")).click();
-
-        $("#city").click();
-        $(byText("Noida")).click();
-        $("#submit").click();
-
-        $(".modal-content").shouldHave(text("Mark"), text("Born"), text("Born@gloom.snail"),
-                text("Male"), text("9500267340"), text("10 December,1988"),
-                text("test.png"), text("Palm Street"),
-                text("NCR"), text("Noida"), text("Thanks for submitting the form"));
+        step("Сверить результаты заполненной формы регистрации", () -> {
+            $(".modal-content").shouldHave(text("Mark"), text("Born"), text("Born@gloom.snail"),
+                    text("Male"), text("9500267340"), text("10 December,1988"),
+                    text("test.png"), text("Palm Street"),
+                    text("NCR"), text("Noida"), text("Thanks for submitting the form"));
+        });
 
 
     }
---
+++ src/test/java/TestBase.java
@@ -0,0 +1,14 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeAll;
+
+public class TestBase {
+    @BeforeAll
+    static void beforeAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1800x900";
+        Configuration.holdBrowserOpen = true;
+    }
+}

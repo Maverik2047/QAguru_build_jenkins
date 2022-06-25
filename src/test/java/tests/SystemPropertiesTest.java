c19677620673f0b475c50c5c12a317f020cfc018
Viacheslav <maverik2047@yandex.ru>
2022-06-25 19:48:32 +0300

    properties added

---
+++ src/test/java/tests/SystemPropertiesTest.java
@@ -0,0 +1,19 @@
+package tests;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+public class SystemPropertiesTest {
+
+    @Test
+    @Tag("test")
+    void propTest() {
+        String browser = System.getProperty("browser", "chrome");
+        String version = System.getProperty("version", "101");
+        String browserSize = System.getProperty("browserSize", "1920x1080");
+
+        System.out.println(browser);
+        System.out.println(version);
+        System.out.println(browserSize);
+    }
+}
